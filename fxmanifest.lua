119293119edf788ab2c4f62d40d31dbe0381d5a5
Daniel Hawton <daniel@hawton.org>
2021-05-02 21:09:59 -0500

    add resource files

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean" 
+game "gta5"
+
+files {
+    "Newtonsoft.Json.dll",
+    "AutoDeleteProShared.net.dll",
+    "*.json"
+}
+
+client_scripts {
+    "AutoDeleteProClient.net.dll"
+}
+
+server_scripts {
+    "AutoDeleteProServer.net.dll"
+}
